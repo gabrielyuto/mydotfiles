db1b82d6caf0de089c9cd739b4ad6032a5b287fb
Gabriel Munakata <gabrielyuto2016@gmail.com>
2025-11-07 12:13:18 -0300

    neovim: add bufferlist plugin and fix mapping

---
+++ nvim/.config/nvim/commands.md
@@ -0,0 +1,19 @@
+# Estudando o neovim
+
+## Modo Visual Block
+CTRL + V: Ativa o modo Visual Block, permitindo selecionar blocos retangulares de texto.
+
+Estando neste modo:
+- Se pressionar 'I' (letra i maiúscula), você pode inserir texto no início de cada linha selecionada.
+- Se pressionar 'A' (letra a maiúscula), você pode adicionar texto ao final de cada linha selecionada.
+- Se pressionar 'D', você pode deletar o bloco selecionado.
+- Se pressionar 'C', você pode substituir o bloco selecionado com novo texto.
+- Se pressionar 'X', você pode cortar o bloco selecionado.
+- Se pressionar 'Y', você pode copiar o bloco selecionado.
+- Se pressionar 'P', você pode colar o conteúdo copiado ou cortado antes do bloco selecionado.
+- Se pressionar 'p', você pode colar o conteúdo copiado ou cortado após o bloco selecionado.
+- Se pressionar 'U', você pode desfazer a última ação realizada no bloco selecionado.
+- Se pressionar 'CTRL + R', você pode refazer a última ação desfeita no bloco selecionado.
+- Após digitar o texto desejado, pressione 'ESC' para aplicar a inserção ou adição em todas as linhas selecionadas.
+
+
--- nvim/.config/nvim/lua/core/mappings.lua
+++ nvim/.config/nvim/lua/core/mappings.lua
@@ -1,61 +1,72 @@
-local api = vim.api
+-- ================================ HELPERS ===================================
+-- n = normal mode
+-- i = insert mode
+-- v = visual mode
+-- x = visual block mode
+-- t = terminal mode
+-- c = command mode
 
+-- ================================ SETTINGS ===================================
 local function map(mode, lhs, rhs, opts)
-  local options = { noremap = true, silent = true }
+  local options = { noremap= true, silent = true }
 
   if opts then
     options = vim.tbl_extend('force', options, opts)
+    vim.api.nvim_set_keymap(mode, lhs, rhs, options)
+  else
+    vim.keymap.set(mode, lhs, rhs)
   end
-
-  api.nvim_set_keymap(mode, lhs, rhs, options)
 end
 
--- Normal Mode
-map('n', '<leader>ff', '<cmd>Telescope find_files<cr>', { desc = 'Find files' })
-map('n', '<leader>fg', '<cmd>Telescope live_grep<cr>', { desc = 'Live grep' })
-map('n', '<leader>fb', '<cmd>Telescope buffers<cr>', { desc = 'List buffers' })
-map('n', '<leader>fh', '<cmd>Telescope help_tags<cr>', { desc = 'Find help' })
-map('n', '<leader>bb', '<cmd>BufferListOpen<cr>', { desc = 'Buffer list' })
-map('n', '<C-/>',      '<cmd>:ToggleTerm direction=float<cr>', { desc = 'Floating Terminal'})
-map('n', '<leader>tv', '<cmd>:ToggleTerm size=70 direction=vertical<cr>', { desc = 'Vertical Terminal' })
-map('n', '<leader>th', '<cmd>:ToggleTerm size=20 direction=horizontal<cr>', {desc = 'Horizontal Terminal'})
-map('n', '<leader>tt', '<cmd>:ToggleTerm direction=tab<cr>', { desc = 'Tab Terminal'})
-map('n', '<leader>e',  '<cmd>:NvimTreeToggle<CR>', { noremap = true, silent = true })
-map('n', '<leader>r',  '<cmd>:NvimTreeRefresh<cr>', { noremap = true, silent = true })
-map('n', '<leader>n',  '<cmd>:NvimTreeFindFile<cr>', { noremap = true, silent = true })
-map('n', '<leader>da', '<cmd>lua require("dapui").eval()<cr>', { desc = 'DAP-UI' })
-map('n','<leader>lg',  '<cmd>LazyGit<cr>', { desc = 'LazyGit' })
-map('n', '<Esc>',      '<cmd>:nohlsearch<cr>', { desc = 'exit find term' })
+-- ============================ MAPPINGS PLUGINS ===============================
+-- GENERAL
+map('n','<Esc>','<cmd>:nohlsearch<cr>',{ desc = 'exit find term' })
+map('t','<Esc>','<C-\\><C-n>',{ desc = 'Change from Terminal to Normal mode' })
+map('v','<C-c>',':w !pbcopy<CR><Esc>',{ desc = 'Copy to transfer area'})
+map('x','<C-i>','I',{ noremap = true, silent = true })
 
--- Terminal Mode
-map('t', '<C-/>', '<C-\\><C-n>:ToggleTerm<cr>', { desc = 'Hide Terminal' })
-map('t', '<Esc>', '<C-\\><C-n>', { desc = 'Change from Terminal to Normal mode' })
+-- TELESCOPE
+map('n','<leader>ff','<cmd>Telescope find_files<cr>',{ desc = 'Find files' })
+map('n','<leader>fg','<cmd>Telescope live_grep<cr>',{ desc = 'Live grep' })
+map('n','<leader>fb','<cmd>Telescope buffers<cr>',{ desc = 'List buffers' })
+map('n','<leader>fh','<cmd>Telescope help_tags<cr>',{ desc = 'Find help' })
 
--- Visual Mode
-map('v', '<C-c>', ':w !pbcopy<CR><Esc>', { desc = 'Copy to transfer area'})
+-- TOGGLETERM
+map('n','<C-/>','<cmd>:ToggleTerm direction=float<cr>',{ desc = 'Floating Terminal'})
+map('n','<leader>tv','<cmd>:ToggleTerm size=70 direction=vertical<cr>',{ desc = 'Vertical Terminal' })
+map('n','<leader>th','<cmd>:ToggleTerm size=20 direction=horizontal<cr>',{desc = 'Horizontal Terminal'})
+map('n','<leader>tt','<cmd>:ToggleTerm direction=tab<cr>',{ desc = 'Tab Terminal'})
+map('t','<C-/>', '<C-\\><C-n>:ToggleTerm<cr>',{ desc = 'Hide Terminal' })
 
--- Visual-only Mode
-map('x', '<C-i>', 'I', { noremap = true, silent = true })
+-- NVIMTREE
+map('n','<leader>e','<cmd>:NvimTreeToggle<CR>',{ noremap = true, silent = true })
+map('n','<leader>r','<cmd>:NvimTreeRefresh<cr>',{ noremap = true, silent = true })
+map('n','<leader>n','<cmd>:NvimTreeFindFile<cr>',{ noremap = true, silent = true })
+
+-- LAZYGIT
+map('n','<leader>lg','<cmd>LazyGit<cr>',{ desc = 'LazyGit' })
+map('n','<leader>lg',':LazyGit<CR>',{ noremap = true, silent = true })
+map('n','<leader>lc',':LazyGitConfig<CR>',{ noremap = true, silent = true })
+map('n','<leader>lf',':LazyGitFilter<CR>',{ noremap = true, silent = true })
+map('n','<leader>lcf',':LazyGitFilterCurrentFile<CR>',{ noremap = true, silent = true })
 
 -- DAP
-vim.fn.sign_define('DapBreakpoint', { text='🛑', texthl='', linehl='', numhl='' })
-vim.keymap.set('n', '<F5>', function() require('dap').continue() end)
-vim.keymap.set('n', '<F10>', function() require('dap').step_over() end)
-vim.keymap.set('n', '<F11>', function() require('dap').step_into() end)
-vim.keymap.set('n', '<F12>', function() require('dap').step_out() end)
-vim.keymap.set('n', '<leader>b', function() require('dap').toggle_breakpoint() end)
-vim.keymap.set('n', '<leader>B', function() require('dap').set_breakpoint(vim.fn.input('Breakpoint condition: ')) end)
-vim.keymap.set('n', '<leader>dr', function() require('dap').repl.open() end)
-vim.keymap.set('n', '<leader>dl', function() require('dap').run_last() end)
+vim.fn.sign_define('DapBreakpoint',{ text='🛑', texthl='', linehl='', numhl='' })
+map('n','<F5>',function() require('dap').continue() end)
+map('n','<F10>',function() require('dap').step_over() end)
+map('n','<F11>',function() require('dap').step_into() end)
+map('n','<F12>',function() require('dap').step_out() end)
+map('n','<leader>b',function() require('dap').toggle_breakpoint() end)
+map('n','<leader>B',function() require('dap').set_breakpoint(vim.fn.input('Breakpoint condition: ')) end)
+map('n','<leader>dr',function() require('dap').repl.open() end)
+map('n','<leader>dl',function() require('dap').run_last() end)
+map('n','<leader>da','<cmd>lua require("dapui").eval()<cr>',{ desc = 'DAP-UI' })
 
--- Atalhos para comandos LazyGit
-vim.keymap.set('n', '<leader>lg', ':LazyGit<CR>', { noremap = true, silent = true })
-vim.keymap.set('n', '<leader>lc', ':LazyGitConfig<CR>', { noremap = true, silent = true })
-vim.keymap.set('n', '<leader>lf', ':LazyGitFilter<CR>', { noremap = true, silent = true })
-vim.keymap.set('n', '<leader>lcf', ':LazyGitFilterCurrentFile<CR>', { noremap = true, silent = true })
+-- VIM-RSPEC
+map('n','<leader>rs','<cmd>:call RunCurrentSpecFile()<cr>',{ desc = 'Find files' })
+map('n','<leader>rn','<cmd>:call RunNearestSpec()<cr>',{ desc = 'Live grep' }) 
+map('n','<leader>rl','<cmd>:call RunLastSpec()<cr>',{ desc = 'List buffers' })
+map('n','<leader>ra','<cmd>:call RunAllSpecs()<cr>',{ desc = 'Find help' })
 
--- vim-rspec mappings
-map('n', '<leader>rs', '<cmd>:call RunCurrentSpecFile()<cr>', { desc = 'Find files' })
-map('n', '<leader>rn', '<cmd>:call RunNearestSpec()<cr>', { desc = 'Live grep' }) 
-map('n', '<leader>rl', '<cmd>:call RunLastSpec()<cr>', { desc = 'List buffers' })
-map('n', '<leader>ra', '<cmd>:call RunAllSpecs()<cr>', { desc = 'Find help' })
+-- BUFFERLIST
+map('n','<leader>bb','<cmd>BufferListOpen<cr>',{ desc = 'Buffer list' })
